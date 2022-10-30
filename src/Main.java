efa4ff9f317f148d73e085adfbbae0f4e8d46bb3
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-10-31 00:06:58 +0300

    commit2-bugsFixed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        int max=0;int min=1000;
+        int piece,number,other=0;
+        System.out.println("Please enter the amount of numbers:");
+        piece= scanner.nextInt();
+        for(int i=1;i<=piece;i++){
+            System.out.println("Please enter the"+" "+i+"."+" "+"number");
+            number= scanner.nextInt();
+
+            if(max<number){max=number;}else if(number<min){min=number;}
+ if(min==1000){min=number;}
+
+
+        }
+        System.out.println("Maximum number:"+max+" "+"Minimum number:"+min);
+
     }
 }
